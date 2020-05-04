2c4c00ce4cb724ea7232c3b6d4d133dc2be342b1
Kenneth Ma <kwyma@uw.edu>
2020-05-03 19:59:24 -0700

    ChainingHashTable.java: implemented all but iterator, all untested

--- src/datastructures/dictionaries/ChainingHashTable.java
+++ src/datastructures/dictionaries/ChainingHashTable.java
@@ -28,32 +28,115 @@ import cse332.interfaces.misc.Dictionary;
 public class ChainingHashTable<K, V> extends DeletelessDictionary<K, V> {
     private Supplier<Dictionary<K, V>> newChain;
 
-    public static final int initSize = 61;
-    public static final int[] NEXT_PRIMES = {127, 251, 503, 1009, 2011, 4027, 8053, 16111, 32261, 65003, 130099, 200003};
+    private static final double GROWTH_RATIO_MAX = 0.8;
+
+    private static final int initSize = 61;
+    private static final int[] NEXT_PRIMES = {127, 251, 503, 1009, 2011, 4027, 8053, 16111, 32261,
+            65003, 130099, 200003};
+    private int nextPrimeIndex;
 
     private Dictionary<K, V>[] hashTable;
 
     public ChainingHashTable(Supplier<Dictionary<K, V>> newChain) {
         this.newChain = newChain;
-        hashTable = (Dictionary<K, V>[]) new Object[initSize];
+        nextPrimeIndex = 0;
+        hashTable = (Dictionary<K, V>[]) new Dictionary[initSize];
     }
 
     @Override
     public V insert(K key, V value) {
+        if(key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+        // check load factor first
+        if(needsRehash()) {
+            rehash();
+        }
+        V prevVal = hashInsert(key, value, hashTable);
+        if(prevVal == null) {
+            size++;
+        }
+        return prevVal;
+    }
+
+    // calculates and returns the hash index for the key based on given length
+    private int getHashIndex(K key, int length) {
         // generate key hash
         int keyHash = key.hashCode();
         // mod by capacity
-        int keyIndex = keyHash %
-        // insert at index
+        return keyHash % length;
+    }
+
+    // helper method — checks and inserts item, returns previous value
+    private V hashInsert (K key, V value, Dictionary<K, V>[] table) {
+        int hashIndex = getHashIndex(key, table.length);
+
+        // check if chain exists at index
+        if(table[hashIndex] == null) {
+            table[hashIndex] = newChain.get();
+        }
+        // get old value
+        // try insert, if already exists, won't add
+        return table[hashIndex].insert(key, value);
+    }
+
+    // checks if load factor is too high
+    private boolean needsRehash() {
+        return ((double) this.size) / hashTable.length > GROWTH_RATIO_MAX;
+    }
+
+    // uses hashInsert to add all elements to new hashTable
+    private void rehash() {
+        // setup new hashTable with ~double capacity
+
+        // init to double size as default
+        int nextTableSize = hashTable.length * 2;
+        // check nextPrimeIndex, if still in table range, use prime size
+        if(nextPrimeIndex < NEXT_PRIMES.length) {
+            nextTableSize = NEXT_PRIMES[nextPrimeIndex];
+            nextPrimeIndex++;
+        }
+        Dictionary<K, V>[] newHashTable =
+                (Dictionary<K, V>[]) new Dictionary[nextTableSize];
+
+        for(Dictionary<K, V> chains : hashTable) {
+            Iterator<Item<K, V>> dictIterator = chains.iterator();
+            while(dictIterator.hasNext()) {
+                Item<K, V> item = iterator().next();
+                hashInsert(item.key, item.value, newHashTable);
+            }
+        }
+        hashTable = newHashTable;
     }
 
     @Override
     public V find(K key) {
-        throw new NotYetImplementedException();
+        if(key == null) {
+            throw new IllegalArgumentException();
+        }
+        int hashIndex = getHashIndex(key, hashTable.length);
+        return hashTable[hashIndex].find(key);
     }
 
     @Override
     public Iterator<Item<K, V>> iterator() {
-        throw new NotYetImplementedException();
+        // check if table index >= length
+        // else, run through current iterator
+        // running out, get next available index's iterator
+        return new ChainingHashTableIterator();
+    }
+
+    private class ChainingHashTableIterator implements Iterator<Item<K, V>> {
+        Item<K, V> curr;
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Item<K, V> next() {
+            return null;
+        }
     }
 }
