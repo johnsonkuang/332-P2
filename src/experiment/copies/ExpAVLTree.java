73cf16637c04018b3ad46764e912f16d7d39da1f
Kenneth Ma <kwyma@uw.edu>
2020-05-08 21:33:28 -0700

    Created copies of AVL, BST, ChainingHT

---
+++ src/experiment/copies/ExpAVLTree.java
@@ -0,0 +1,133 @@
+package experiment.copies;
+
+import cse332.datastructures.trees.BinarySearchTree;
+
+/**
+ * TODO: Replace this comment with your own as appropriate.
+ *
+ * AVLTree must be a subclass of BinarySearchTree<E> and must use
+ * inheritance and calls to superclass methods to avoid unnecessary
+ * duplication or copying of functionality.
+ *
+ * 1. Create a subclass of BSTNode, perhaps named AVLNode.
+ * 2. Override the insert method such that it creates AVLNode instances
+ *    instead of BSTNode instances.
+ * 3. Do NOT "replace" the children array in BSTNode with a new
+ *    children array or left and right fields in AVLNode.  This will 
+ *    instead mask the super-class fields (i.e., the resulting node 
+ *    would actually have multiple copies of the node fields, with 
+ *    code accessing one pair or the other depending on the type of 
+ *    the references used to access the instance).  Such masking will 
+ *    lead to highly perplexing and erroneous behavior. Instead, 
+ *    continue using the existing BSTNode children array.
+ * 4. If this class has redundant methods, your score will be heavily
+ *    penalized.
+ * 5. Cast children array to AVLNode whenever necessary in your
+ *    AVLTree. This will result a lot of casts, so we recommend you make
+ *    private methods that encapsulate those casts.
+ * 6. Do NOT override the toString method. It is used for grading.
+ */
+
+public class ExpAVLTree<K extends Comparable<? super K>, V> extends BinarySearchTree<K, V>  {
+
+    private AVLNode current;
+
+    public ExpAVLTree(){
+        super();
+    }
+
+    @Override
+    public V insert(K key, V value) {
+        if(key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.root = insertFind(key, (AVLNode) this.root);
+        V oldValue = current.value;
+        current.value = value;
+        return oldValue;
+    }
+
+    private AVLNode insertFind(K key, AVLNode node){
+        //insert, recurse to the bottom of the tree
+        if(node == null) {
+            current = new AVLNode(key, null);
+            size++;
+            return current;
+        } else if(key.compareTo(node.key) > 0){
+            node.children[1] = insertFind(key, node.castChildrenToAVL(1));
+        } else if(key.compareTo(node.key) < 0){
+            node.children[0] = insertFind(key, node.castChildrenToAVL(0));
+        } else {
+            current = node;
+            return current;
+        }
+        // rebalance: perform rotation and update height
+        return balanceTree(node);
+    }
+
+    private int getHeight(AVLNode node){
+        return node == null ? -1 : node.height;
+    }
+
+    private void updateHeight(AVLNode node){
+        node.height = 1 + Math.max(getHeight(node.castChildrenToAVL(0)),
+                                   getHeight(node.castChildrenToAVL(1)));
+    }
+
+    private int getBalance(AVLNode node){
+        return (node == null) ? 0 :
+                getHeight(node.castChildrenToAVL(0)) - getHeight(node.castChildrenToAVL(1));
+    }
+
+    private AVLNode balanceTree(AVLNode node){
+        updateHeight(node);
+
+        int balVal = getBalance(node);
+        if(balVal > 1) {                        // go left first
+            int leftBalVal = getBalance(node.castChildrenToAVL(0));
+            if(leftBalVal < 0) {                // left-right => double rot
+                node.children[0] = rotate(node.castChildrenToAVL(0), false);
+            }                                   // rot for single and double rot
+            node = rotate(node, true);
+        } else if(balVal < -1) {                // go right first
+            int rightBalVal = getBalance(node.castChildrenToAVL(1));
+            if(rightBalVal > 0) {                // right-left => double rot
+                node.children[1] = rotate(node.castChildrenToAVL(1), true);
+            }                                   // rot for single and double rot
+            node = rotate(node, false);
+        }
+
+        return node;
+    }
+
+    public AVLNode rotate (AVLNode parent, boolean isRight) {
+        // For rotate right (opp. for rotate left): ref to left child
+        AVLNode temp = parent.castChildrenToAVL(isRight ? 0 : 1);
+        // For rotate right (opp. for rotate left): set parent left ref to left child's right child
+        parent.children[isRight ? 0 : 1] = temp.castChildrenToAVL(isRight ? 1 : 0);
+        // For rotate right (opp. for rotate left): set child's right to parent (child has moved up)
+        temp.children[isRight? 1 : 0] = parent;
+
+        // update heights
+        updateHeight(parent);
+        updateHeight(temp);
+
+        return temp;
+    }
+
+    //TODO: rotateLeft, Right, Double Rotate, insert (will have find)
+
+    private class AVLNode extends BSTNode{
+        public int height;
+
+        public AVLNode(K key, V value){
+            super(key, value);
+            height = 0;
+        }
+
+        public AVLNode castChildrenToAVL (int childIndex) {
+            return (AVLNode) this.children[childIndex];
+        }
+    }
+}
---
+++ src/experiment/copies/ExpChainingHashTable.java
@@ -0,0 +1,170 @@
+package experiment.copies;
+
+import cse332.datastructures.containers.Item;
+import cse332.interfaces.misc.DeletelessDictionary;
+import cse332.interfaces.misc.Dictionary;
+
+import java.util.Iterator;
+import java.util.function.Supplier;
+
+/**
+ * TODO: Replace this comment with your own as appropriate.
+ * 1. You must implement a generic chaining hashtable. You may not
+ *    restrict the size of the input domain (i.e., it must accept 
+ *    any key) or the number of inputs (i.e., it must grow as necessary).
+ * 3. Your HashTable should rehash as appropriate (use load factor as
+ *    shown in class!). 
+ * 5. HashTable should be able to resize its capacity to prime numbers for more 
+ *    than 200,000 elements. After more than 200,000 elements, it should 
+ *    continue to resize using some other mechanism.
+ * 6. We suggest you hard code some prime numbers. You can use this
+ *    list: http://primes.utm.edu/lists/small/100000.txt 
+ *    NOTE: Do NOT copy the whole list!
+ * 7. When implementing your iterator, you should NOT copy every item to another
+ *    dictionary/list and return that dictionary/list's iterator. 
+
+ */
+public class ExpChainingHashTable<K, V> extends DeletelessDictionary<K, V> {
+    private Supplier<Dictionary<K, V>> newChain;
+
+    private static final double GROWTH_RATIO_MAX = 0.8;
+
+    private static final int initSize = 61;
+    private static final int[] NEXT_PRIMES = {127, 251, 503, 1009, 2011, 4027, 8053, 16111, 32261,
+            65003, 130099, 200003};
+    private int nextPrimeIndex;
+
+    private Dictionary<K, V>[] hashTable;
+
+    public ExpChainingHashTable(Supplier<Dictionary<K, V>> newChain) {
+        this.newChain = newChain;
+        nextPrimeIndex = 0;
+        hashTable = (Dictionary<K, V>[]) new Dictionary[initSize];
+    }
+
+    @Override
+    public V insert(K key, V value) {
+        if(key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+        // check load factor first
+        if(needsRehash()) {
+            rehash();
+        }
+        V prevVal = hashInsert(key, value, hashTable);
+        if(prevVal == null) {
+            size++;
+        }
+        return prevVal;
+    }
+
+    // calculates and returns the hash index for the key based on given length
+    private int getHashIndex(K key, int length) {
+        // generate key hash
+        int keyHash = key.hashCode();
+        // mod by capacity
+        return keyHash % length;
+    }
+
+    // helper method — checks and inserts item, returns previous value
+    private V hashInsert (K key, V value, Dictionary<K, V>[] table) {
+        int hashIndex = getHashIndex(key, table.length);
+
+        // check if chain exists at index
+        if(table[hashIndex] == null) {
+            table[hashIndex] = newChain.get();
+        }
+        // get old value
+        // try insert, if already exists, won't add
+        return table[hashIndex].insert(key, value);
+    }
+
+    // checks if load factor is too high
+    private boolean needsRehash() {
+        return ((double) this.size) / hashTable.length > GROWTH_RATIO_MAX;
+    }
+
+    // uses hashInsert to add all elements to new hashTable
+    private void rehash() {
+        // setup new hashTable with ~double capacity
+
+        // init to double size as default
+        int nextTableSize = hashTable.length * 2;
+        // check nextPrimeIndex, if still in table range, use prime size
+        if(nextPrimeIndex < NEXT_PRIMES.length) {
+            nextTableSize = NEXT_PRIMES[nextPrimeIndex];
+            nextPrimeIndex++;
+        }
+        Dictionary<K, V>[] newHashTable =
+                (Dictionary<K, V>[]) new Dictionary[nextTableSize];
+
+        for(Dictionary<K, V> chains : hashTable) {
+            if(chains != null) {
+                for(Item<K, V> item : chains) {
+                    hashInsert(item.key, item.value, newHashTable);
+                }
+            }
+        }
+        hashTable = newHashTable;
+    }
+
+    @Override
+    public V find(K key) {
+        if(key == null) {
+            throw new IllegalArgumentException();
+        }
+        int hashIndex = getHashIndex(key, hashTable.length);
+        if(hashTable[hashIndex] != null) {
+            return hashTable[hashIndex].find(key);
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<Item<K, V>> iterator() {
+        // check if table index >= length
+        // else, run through current iterator
+        // running out, get next available index's iterator
+        return new ChainingHashTableIterator();
+    }
+
+    private class ChainingHashTableIterator implements Iterator<Item<K, V>> {
+        Item<K, V> curr;
+        private int hashTableIndex = 0;
+        private int itemsSeen = 0;
+        private Iterator<Item<K, V>> dictItr;
+
+        public ChainingHashTableIterator() {
+            dictItr = getNextIterator();
+        }
+
+        private Iterator<Item<K, V>> getNextIterator() {
+            // loop through hashTable until dict (with iterator) is found
+            while(hashTableIndex < ExpChainingHashTable.this.hashTable.length &&
+                  ExpChainingHashTable.this.hashTable[hashTableIndex] == null){
+                hashTableIndex++;
+            }
+            if(hashTableIndex == ExpChainingHashTable.this.hashTable.length){
+                throw new IndexOutOfBoundsException();
+            }
+            //invariant: hashTableIndex points to the index of the first dictionary in the hashtable
+            return ExpChainingHashTable.this.hashTable[hashTableIndex].iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return itemsSeen < ExpChainingHashTable.this.size();
+        }
+
+        @Override
+        public Item<K, V> next() {
+            // if current dict iterator empty, use index to search for next iterator
+            if(!dictItr.hasNext()) {
+                hashTableIndex++;
+                dictItr = getNextIterator();
+            }
+            itemsSeen++;
+            return dictItr.next();
+        }
+    }
+}
