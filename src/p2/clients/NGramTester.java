a21d890e595e47b9c24711d26128476903bcbd3f
Johnson Kuang <johnsonkuang@outlook.com>
2020-05-09 15:44:10 -0700

    moved testing code from NgramTester to ExpNgramTester, reverted NGramTester to normal

--- src/p2/clients/NGramTester.java
+++ src/p2/clients/NGramTester.java
@@ -1,8 +1,5 @@
 package p2.clients;
 
-import java.io.IOException;
-import java.util.function.Supplier;
-
 import cse332.datastructures.trees.BinarySearchTree;
 import cse332.interfaces.misc.BString;
 import cse332.interfaces.misc.Dictionary;
@@ -13,7 +10,8 @@ import datastructures.dictionaries.ChainingHashTable;
 import datastructures.dictionaries.HashTrieMap;
 import p2.wordsuggestor.WordSuggestor;
 
-import javax.sound.midi.SysexMessage;
+import java.io.IOException;
+import java.util.function.Supplier;
 
 public class NGramTester {
     public static <A extends Comparable<A>, K extends BString<A>, V> Supplier<Dictionary<K, V>> trieConstructor(Class<K> clz) {
@@ -24,75 +22,24 @@ public class NGramTester {
             Supplier<Dictionary<K, V>> constructor) {
         return () -> new ChainingHashTable<K, V>(constructor);
     }
-    
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public static <K, V> Supplier<Dictionary<K, V>> binarySearchTreeConstructor() {
         return () -> new BinarySearchTree();
     }
-    
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public static <K, V> Supplier<Dictionary<K, V>> avlTreeConstructor() {
         return () -> new AVLTree();
     }
 
-    public enum dicts {
-        hashTrieMap, hashTable, bst, avl
-    }
-
-    public static final int NUM_TESTS = 8;
-    public static final int NUM_WARMUP = 3;
 
     public static void main(String[] args) {
         try {
-            StringBuilder output = new StringBuilder();
-            for(dicts dict: dicts.values()){
-                output.append("---------------------------------------\n");
-                output.append("Results for " + dict.name() + "\n");
-
-                double totalAddTime = 0;
-                double totalFindTime = 0;
-
-                for(int trial = 1; trial <= NUM_TESTS; trial++){
-                    //start add time
-
-                    WordSuggestor suggestions;
-                    //start time
-                    long addStartTime = System.nanoTime();
-                    switch (dict) {
-                        case hashTrieMap:
-                            suggestions = new WordSuggestor("alice.txt", 3, -1,
-                                    NGramTester.trieConstructor(NGram.class),
-                                    NGramTester.trieConstructor(AlphabeticString.class));
-                            break;
-                        case hashTable:
-                            suggestions = new WordSuggestor("alice.txt", 3, -1,
-                                    NGramTester.hashtableConstructor(() -> new AVLTree()),
-                                    NGramTester.hashtableConstructor(() -> new AVLTree()));
-                            break;
-                        case bst:
-                            suggestions = new WordSuggestor("alice.txt", 3, -1,
-                                    NGramTester.binarySearchTreeConstructor(),
-                                    NGramTester.binarySearchTreeConstructor());
-                            break;
-                        case avl:
-                            suggestions = new WordSuggestor("alice.txt", 3, -1,
-                                    NGramTester.avlTreeConstructor(),
-                                    NGramTester.avlTreeConstructor());
-                    }
-                    //end time
-                    long addEndTime = System.nanoTime();
-                    //in milliseconds
-                    double addTime = (addEndTime - addStartTime) / 1_000_000.0;
-
-                    if(trial > NUM_WARMUP){
-                        output.append("\tTrial " + (trial - NUM_WARMUP) + ":\n");
-                        output.append("\t\tAdd Time: " + addTime + " ms\n");
-                        totalAddTime += addTime;
-                    }
-                    //find component
-
-                }
-            }
+            WordSuggestor suggestions = new WordSuggestor("eggs.txt", 2, -1,
+                    experiment.copies.ExpNGramTester.trieConstructor(NGram.class),
+                    experiment.copies.ExpNGramTester.trieConstructor(AlphabeticString.class));
+            System.out.println(suggestions);
         } catch (IOException e) {
             e.printStackTrace();
         }
